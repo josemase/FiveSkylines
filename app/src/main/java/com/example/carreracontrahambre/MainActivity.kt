62f153570ef77f7e9e2a2c20bd82fb36084ad907
Johann <johanngago@unisabana.edu.co>
2020-10-10 19:12:25 -0500

    s

--- app/src/main/java/com/example/carreracontrahambre/MainActivity.kt
+++ app/src/main/java/com/example/carreracontrahambre/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.carreracontrahambre
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import com.example.carreracontrahambre.carrera.CarreraFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, CarreraFragment())
+            .commit()
+        return super.onCreateView(name, context, attrs)
+    }
 
 }
--- app/src/main/java/com/example/carreracontrahambre/estudiante/RegisterFragment.kt
+++ app/src/main/java/com/example/carreracontrahambre/estudiante/RegisterFragment.kt
@@ -1,12 +1,19 @@
 package com.example.carreracontrahambre.estudiante
 
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.carreracontrahambre.R
+import com.example.carreracontrahambre.carrera.CarreraFragment
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_register.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,7 +29,7 @@ class RegisterFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var  mAuth: FirebaseAuth;
+    private lateinit var  auth: FirebaseAuth;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -38,9 +45,37 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
-
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance();
+        buttonRegister.setOnClickListener(){
+            auth.createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
+                .addOnCompleteListener(activity as Activity) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        Toast.makeText(context,"U Signed Up successfully",Toast.LENGTH_LONG).show();
+                        activity.supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainer, CarreraFragment())
+                            .commit()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText( context, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+
+                    // ...
+                }
+        }
 
     }
+
+
 }
